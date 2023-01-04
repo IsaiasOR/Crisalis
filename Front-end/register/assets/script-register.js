9daefcef01bfb4f46552c969acf79b92400312ec
Isaías <isa.reniero001@hotmail.com>
2023-01-04 10:32:00 -0300

    Probando angular

--- Front-end/register/assets/script-register.js
+++ Front-end/register/assets/script-register.js
@@ -27,3 +27,24 @@ formElement.addEventListener('submit', (event) => {
         .then(json => console.log(json))
         .catch(error => console.log(error));
 })
+
+
+/* add hovered class in selected list item */
+let list = document.querySelectorAll('.navigation li');
+function activeLink() {
+    list.forEach((item) => 
+    item.classList.remove(''));
+    this.classList.add('hovered');
+}
+list.forEach((item ) => 
+item.addEventListener('mouseover', activeLink));
+
+/* Menu toggle */
+let toggle = document.querySelector('.toggle');
+let navigation = document.querySelector('.navigation');
+let topB = document.querySelector('.top');
+
+toggle.onclick = function() {
+    navigation.classList.toggle('active');
+    topB.classList.toggle('active');
+}
